eba5378c39f2396cb448a5d5087e0beb69908fb2
Origier <sergeantbeast456@gmail.com>
2018-12-27 00:29:46 -0500

    removed useless code

--- main.rb
+++ main.rb
@@ -2,9 +2,7 @@ def stock_picker(array)
   buy_day = 0
   sell_day = 0
   array.length.times do |i|
-    if array[i - 1] == nil
-      buy_day = i
-    elsif (array[i] < array[buy_day]) and (array[i] != array[buy_day])
+    if (array[i] < array[buy_day]) and (array[i] != array[buy_day])
       buy_day = i
     elsif (array[sell_day] < array[i]) and (array[i] != array[sell_day])
       sell_day = i
